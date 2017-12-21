058c4c01bf544b2c6cfb4621474d44b3b5d75232
Ina Chemerko <innachemerko@gmail.com>
2017-12-21 04:00:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,8 +14,9 @@ function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
   object[key] = value;
   return object;
 }
-
+ 
 function deleteFromObjectByKey(object, key){
+  var newObject = Object.assign({}, Object);
   
 }
 
