5fbcc345da9b01f2479cc861942f8d93e8fcf102
Ina Chemerko <innachemerko@gmail.com>
2017-12-21 04:12:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,11 +9,17 @@ var recipes = {
   var newObject = Object.assign({}, object);
   newObject[key] = value;
  return newObject;
-   }*/
+   }
 function updateObjectWithKeyAndValue(object, key, value) {
   var newObject = Object.assign({}, object, { [key]: value });
  return newObject;
+   }*/
+   
+   function updateObjectWithKeyAndValue(object, key, value) {
+  
+ return Object.assign({}, object, {[key]: value});
    }
+   
    
    
 function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
